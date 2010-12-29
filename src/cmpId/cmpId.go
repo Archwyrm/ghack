64a74953abba9fc40663a9065b6bc19bdcb1659e
Markus Martin <markus@archwyrm.net>
2010-12-29 02:27:06 -0700

    Added README, license file, and license headers

--- src/cmpId/cmpId.go
+++ src/cmpId/cmpId.go
@@ -1,3 +1,7 @@
+// Copyright 2010 The ghack Authors. All rights reserved.
+// Use of this source code is governed by the GNU General Public License
+// version 3 (or any later version). See the file COPYING for details.
+
 // Namespace for component indentification in order to ensure uniqueness
 // in bookkeeping.
 package cmpId
--- src/main/component.go
+++ src/main/component.go
@@ -1,3 +1,7 @@
+// Copyright 2010 The ghack Authors. All rights reserved.
+// Use of this source code is governed by the GNU General Public License
+// version 3 (or any later version). See the file COPYING for details.
+
 // Contains everything necessary for component system
 
 package main
