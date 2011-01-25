cfaff31a79a75b79d98e751cf548dcd5035020fb
Owen Raccuglia <ojracc@gmail.com>
2011-01-24 22:42:53 -0500

    A potential implementation of the inner loop.
    I don't know what architecture you had in mind for the game's run loop,
    but I'm having trouble thinking about how comm will handle messages
    without any structure, so I took a stab at it.
    
    Note lines 58 through 67; either I'm using pubsub wrong, or if I'm
    right, we've run into a situation where Go's lack of generics hurts us.
    It feels awkward to make statements this strong in a project I'm
    completely new to, and in a language I'm not fluent in. So, with that in
    mind, here's why I think pubsub needs to run on a chan CmpMsg (or some
    similar interface), at least until Go supports generics. (Fingers
    crossed.)
    
    I think we want to be able to subscribe an Entity object to a PubSub
    topic. But, we can't cast the CmpMsg channel to interface{} (because
    while a CmpMsg implements interface{}, the converse isn't true). So,
    that means PubSub can only allow communication between typeless
    channels. Since I assume we want *some* type-safety, I think that means
    PubSub needs to change its channel type.

--- src/main/game.go
+++ src/main/game.go
@@ -8,41 +8,67 @@ import (
     "fmt"
     "time"
     "cmpId/cmpId"
+    "core/core"
+    "pubsub/pubsub"
 )
 
 // The core of the game
 type Game struct {
     *CmpData
+    pubSub chan core.ServiceMsg
 }
 
 func (g Game) Id() int      { return cmpId.Game }
 func (g Game) Name() string { return "Game" }
-func NewGame() *Game        { return &Game{NewCmpData()} }
+func NewGame() *Game        { return &Game{NewCmpData(), make(chan core.ServiceMsg)} }
 
 // TODO: Move functionality to an Init Action
 func (g Game) GameLoop() {
     // Initialize stuff
-    entities := NewEntityList()
-    g.states[entities.Id()] = entities
+    g.Initialize()
 
-    player := NewPlayer()
-    entities.Entities[player.Name()] = player
-    playerChan := make(chan CmpMsg)
-    go player.Run(playerChan)
-
-    msg := MsgAddAction{&Move{1, 1}}
-    playerChan <- msg
-
-    reply := make(chan State)
-    msg2 := MsgGetState{cmpId.Position, reply}
-    tick_msg := MsgTick{}
-    playerChan <- tick_msg // Tick once
+    getState := make(chan State)
+    go func() {
+        for {
+            pos := (<-getState).(Position)
+            fmt.Printf("Position is currently: %d, %d\n", pos.X, pos.Y)
+        }
+    }()
 
     for {
-        playerChan <- msg2
-        pos := (<-reply).(Position)
-        fmt.Printf("Position is currently: %d, %d\n", pos.X, pos.Y)
-        playerChan <- tick_msg
+        g.publish("entities", MsgTick{})
+        g.publish("entities", MsgGetState{cmpId.Position, getState})
         time.Sleep(5e9) // 3s
     }
+}
+
+// This will happen a lot; might want it to be public
+func (g Game) publish(topic string, data interface{}) {
+    g.pubSub <- pubsub.PublishMsg{Topic: topic, Data: data}
+}
+
+func (g Game) Initialize() {
+    entities := NewEntityList()
+    g.states[entities.Id()] = entities
+    go pubsub.NewPubSub().Run(g.pubSub)
+
+    // The rest is for demo purposes
+    player := NewPlayer()
+    playerChan := make(chan CmpMsg)
+    glueChan := make(chan interface{})
+    go func() {
+        // This was interesting - directly passing playerChan to pubsub broke, saying
+        // cannot use playerChan (type chan CmpMsg) as type chan interface { } in field value
+        for {
+            cmpMsg, ok := (<-glueChan).(CmpMsg)
+            if ok {
+                playerChan <- cmpMsg
+            }
+        }
+    }()
+    g.pubSub <- pubsub.SubscribeMsg{"entities", glueChan}
+    go player.Run(playerChan)
+    entities.Entities[player.Name()] = player
+
+    g.publish("entities", MsgAddAction{&Move{1, 1}})
 }
