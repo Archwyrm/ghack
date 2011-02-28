418280820dd64905c1b692f2ad5e5ab662c0f10a
Markus Martin <markus@archwyrm.net>
2011-02-28 00:53:23 -0700

    Implemented sending repeated StateValues
     * Supports sending states containing slices
     * Supports states with multiple fields

--- src/comm/messages.go
+++ src/comm/messages.go
@@ -96,18 +96,27 @@ func makeUpdateState(id int32, stateId string, value *protocol.StateValue) (msg 
 }
 
 // Creates the right type of StateValue message for an arbitrary State type.
-func packState(state core.State) *protocol.StateValue {
-    msg := &protocol.StateValue{} // Init message
-    t := reflect.NewValue(state)
-    state_v, ok := t.(*reflect.StructValue)
+func packState(state core.State) (msg *protocol.StateValue) {
+    val := reflect.NewValue(state)
+    state_v, ok := val.(*reflect.StructValue)
     if !ok {
         panic("State is non-struct type!")
     }
-    if state_v.NumField() > 1 {
-        panic("Protocol only supports states with one field currently")
+
+    field_num := state_v.NumField()
+    if field_num > 1 { // If we have multiple fields, treat as array
+        msg = &protocol.StateValue{}
+        msg.Type = protocol.NewStateValue_Type(protocol.StateValue_ARRAY)
+        msg.ArrayVal = makeStateValueArray(state_v, field_num)
+    } else { // Single field
+        msg = readField(state_v.Field(0))
     }
-    // TODO: Add support for state structs with more than one field
-    val := state_v.Field(0)
+    return msg
+}
+
+// Reads a single arbitrary type and returns the proper StateValue.
+func readField(val reflect.Value) *protocol.StateValue {
+    msg := &protocol.StateValue{}
     switch f := val.(type) {
     case *reflect.BoolValue:
         msg.Type = protocol.NewStateValue_Type(protocol.StateValue_BOOL)
@@ -122,9 +131,32 @@ func packState(state core.State) *protocol.StateValue {
         msg.Type = protocol.NewStateValue_Type(protocol.StateValue_STRING)
         msg.StringVal = proto.String(f.Get())
     case *reflect.SliceValue:
-        //TODO: Implement repeated values
+        msg.Type = protocol.NewStateValue_Type(protocol.StateValue_ARRAY)
+        msg.ArrayVal = makeStateValueArray(f, f.Len())
     default:
         panic("State value not supported:" + val.Type().String())
     }
     return msg
+}
+
+// Creates a slice of StateValues based on multiple arbitrary types.
+func makeStateValueArray(value reflect.Value, num int) []*protocol.StateValue {
+    msg_array := make([]*protocol.StateValue, 0, num)
+
+    // One of these will be valid, the other will not
+    struct_v, ok := value.(*reflect.StructValue)
+    slice_v, ok2 := value.(*reflect.SliceValue)
+    _, _ = ok, ok2 // These can be safely ignored
+
+    // Loop through the struct's fields
+    for i := 0; i < num; i++ {
+        var val reflect.Value
+        if struct_v != nil {
+            val = struct_v.Field(i)
+        } else {
+            val = slice_v.Elem(i)
+        }
+        msg_array = append(msg_array, readField(val))
+    }
+    return msg_array
 }
