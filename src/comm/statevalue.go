ee9c4ded469281740f2e0f4afe33fcf0a30be30e
Markus Martin <markus@archwyrm.net>
2011-03-04 00:23:21 -0700

    Added tests for state packing code
    This code has also been moved out of messages.go and into its own file
    as it really didn't have much in common with the former and that file
    will no doubt grow a good deal yet.

---
+++ src/comm/statevalue.go
@@ -0,0 +1,78 @@
+// Copyright 2011 The ghack Authors. All rights reserved.
+// Use of this source code is governed by the GNU General Public License
+// version 3 (or any later version). See the file COPYING for details.
+
+package comm
+
+import (
+    "reflect"
+    "core/core"
+    "protocol/protocol"
+    "goprotobuf.googlecode.com/hg/proto"
+)
+
+// Creates the right type of StateValue message for an arbitrary State type.
+func packState(state core.State) (msg *protocol.StateValue) {
+    val := reflect.NewValue(state)
+    state_v, ok := val.(*reflect.StructValue)
+    if !ok {
+        panic("State is non-struct type!")
+    }
+
+    field_num := state_v.NumField()
+    if field_num > 1 { // If we have multiple fields, treat as array
+        msg = &protocol.StateValue{}
+        msg.Type = protocol.NewStateValue_Type(protocol.StateValue_ARRAY)
+        msg.ArrayVal = makeStateValueArray(state_v, field_num)
+    } else { // Single field
+        msg = readField(state_v.Field(0))
+    }
+    return msg
+}
+
+// Reads a single arbitrary type and returns the proper StateValue.
+func readField(val reflect.Value) *protocol.StateValue {
+    msg := &protocol.StateValue{}
+    switch f := val.(type) {
+    case *reflect.BoolValue:
+        msg.Type = protocol.NewStateValue_Type(protocol.StateValue_BOOL)
+        msg.BoolVal = proto.Bool(f.Get())
+    case *reflect.IntValue:
+        msg.Type = protocol.NewStateValue_Type(protocol.StateValue_INT)
+        msg.IntVal = proto.Int(int(f.Get()))
+    case *reflect.FloatValue:
+        msg.Type = protocol.NewStateValue_Type(protocol.StateValue_FLOAT)
+        msg.FloatVal = proto.Float32(float32(f.Get()))
+    case *reflect.StringValue:
+        msg.Type = protocol.NewStateValue_Type(protocol.StateValue_STRING)
+        msg.StringVal = proto.String(f.Get())
+    case *reflect.SliceValue:
+        msg.Type = protocol.NewStateValue_Type(protocol.StateValue_ARRAY)
+        msg.ArrayVal = makeStateValueArray(f, f.Len())
+    default:
+        panic("State value not supported:" + val.Type().String())
+    }
+    return msg
+}
+
+// Creates a slice of StateValues based on multiple arbitrary types.
+func makeStateValueArray(value reflect.Value, num int) []*protocol.StateValue {
+    msg_array := make([]*protocol.StateValue, 0, num)
+
+    // One of these will be valid, the other will not
+    struct_v, ok := value.(*reflect.StructValue)
+    slice_v, ok2 := value.(*reflect.SliceValue)
+    _, _ = ok, ok2 // These can be safely ignored
+
+    // Loop through the struct's fields
+    for i := 0; i < num; i++ {
+        var val reflect.Value
+        if struct_v != nil {
+            val = struct_v.Field(i)
+        } else {
+            val = slice_v.Elem(i)
+        }
+        msg_array = append(msg_array, readField(val))
+    }
+    return msg_array
+}
---
+++ src/comm/statevalue_test.go
@@ -0,0 +1,84 @@
+// Copyright 2011 The ghack Authors. All rights reserved.
+// Use of this source code is governed by the GNU General Public License
+// version 3 (or any later version). See the file COPYING for details.
+
+// Tests that reflection based processing on various types of core.State creates
+// the right messages to send to clients.
+
+package comm
+
+import (
+    "testing"
+    "reflect"
+    "core/core"
+    "protocol/protocol"
+    "goprotobuf.googlecode.com/hg/proto"
+)
+
+type multipleFieldState struct {
+    Value1 int
+    Value2 int
+}
+
+func (x multipleFieldState) Id() core.StateId { return 1 }
+func (x multipleFieldState) Name() string     { return "MultipleFieldState" }
+
+type sliceFieldState struct {
+    Value []int
+}
+
+func (x sliceFieldState) Id() core.StateId { return 2 }
+func (x sliceFieldState) Name() string     { return "SliceFieldState" }
+func newSliceFieldState(s []int) *sliceFieldState { return &sliceFieldState{s}}
+
+func TestSingleFieldState(t *testing.T) {
+    // testState is made available by observer_test.go
+    state := testState{9}
+    sv := &protocol.StateValue{}
+    sv.Type = protocol.NewStateValue_Type(protocol.StateValue_INT)
+    sv.IntVal = proto.Int(state.Value)
+
+    equalOrError(t, sv, packState(state))
+}
+
+func TestMultipleFieldState(t *testing.T) {
+    state := multipleFieldState{1, 2}
+    sv := &protocol.StateValue{}
+    sv.Type = protocol.NewStateValue_Type(protocol.StateValue_ARRAY)
+
+    inner_v1 := makeIntValMsg(1)
+    inner_v2 := makeIntValMsg(2)
+    sv.ArrayVal = append([]*protocol.StateValue{}, inner_v1, inner_v2)
+
+    equalOrError(t, sv, packState(state))
+}
+
+func TestSliceFieldState(t *testing.T) {
+    s := []int{1, 2, 3, 4, 5}
+    state := sliceFieldState{s}
+    sv := &protocol.StateValue{}
+    sv.Type = protocol.NewStateValue_Type(protocol.StateValue_ARRAY)
+
+    msgs := make([]*protocol.StateValue, 0, len(s))
+    for _, i := range s {
+        msgs = append(msgs, makeIntValMsg(i))
+    }
+    sv.ArrayVal = msgs
+
+    equalOrError(t, sv, packState(state))
+}
+
+// Returns an error if two StateValues are not equal
+func equalOrError(t *testing.T, sv, pack interface{}) {
+    if !reflect.DeepEqual(sv, pack) {
+        t.Error("Test state and packed state value messages do not match!")
+    }
+}
+
+// Returns a StateValue with IntVal set to the passed value x
+func makeIntValMsg(x int) *protocol.StateValue {
+    sv := &protocol.StateValue{}
+    sv.Type = protocol.NewStateValue_Type(protocol.StateValue_INT)
+    sv.IntVal = proto.Int(x)
+    return sv
+}
