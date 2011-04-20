00f1b969d47ba63285518d42e857ffd576910d79
Markus Martin <markus@archwyrm.net>
2011-04-20 01:15:43 -0600

    Fixed MsgBuffer sending behavior and simplified func
    The last implementation would only send a value when it received one
    which was filling the buffer pretty quickly (depending on the volume of
    messages received). This current implementation is simpler, but has the
    drawback of always putting the received message in the queue.
    
    The last implementation also had a bug where it would miss the
    occasional message. This had the effect of preventing the player from
    moving when other messages were being sent to World.

--- src/util/util.go
+++ src/util/util.go
@@ -26,29 +26,29 @@ func DrainUntilQuit(ch chan core.Msg) {
 }
 
 // Variable length buffer for the passed channel. Returns a channel for input.
+// TODO: Write test to ensure that this function always does the right thing
 func MsgBuffer(ch chan core.Msg) chan core.Msg {
     in := make(chan core.Msg)
     go func() {
         buf := make([]core.Msg, 0, 2)
         var msg core.Msg
         var out chan core.Msg // Start as nil so we don't send
+
+        // Alternate between receiving from in and sending on out. Each
+        // received message gets appended onto buf and then the first value is
+        // popped off and sent.
         for {
+            if len(buf) == 0 {
+                out = nil // Disable send
+            } else {
+                out = ch // Enable send
+                msg = buf[0] // Set value to send
+                buf = buf[1:len(buf)] // Discard the value
+            }
             select {
             case next := <-in: // Read next message
-                out = ch // Enable send
-                if msg != nil {
-                    buf = append(buf, next) // Save in queue
-                } else if len(buf) > 0 {
-                    msg = buf[0] // Buf has messages, pop the next in queue
-                    buf = buf[1:len(buf)]
-                } else {
-                    msg = next
-                }
-            case out <- msg: // Try to send current msg
-                msg = nil // Mark msg as sent
-                if len(buf) == 0 {
-                    out = nil // Disable send
-                }
+                buf = append(buf, next) // Save in queue
+            case out <- msg: // Send message, if enabled
             }
         }
     }()
